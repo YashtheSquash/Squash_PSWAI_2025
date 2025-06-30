6e9022fe635a010b2c172c6373e85fc07233bfcb
Yash Singh <Yashsingh4496@gmail.com>
2025-06-30 14:48:19 -0400

    This is statistics stuff

---
+++ practices/statistics.py
@@ -0,0 +1,14 @@
+user_input = input("Enter numbers separated by commas: ")
+user_input = user_input.split(',')
+numbers = [int(num)for num in user_input]
+numbers.sort()
+
+Length = len(numbers)
+if Length % 2 == 0:
+    median1 = numbers[Length // 2]
+    median2 = numbers[Length // 2 - 1]
+    median = (median1 + median2) / 2
+else:
+    median = numbers[Length // 2]
+
+print("The median is:", median)
